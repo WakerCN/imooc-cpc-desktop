6776d27d1695e2e11dbf142f324609ac26047133
StarOne <1209562577@qq.com>
2025-07-11 17:28:43 +0800

    ✨ feat <init> init project

---
+++ cross-platform-chat/vite.renderer.config.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author       : 魏威
+ * @Date         : 2025-07-11 11:55
+ * @LastEditTime : 2025-07-11 17:17
+ * @LastEditors  : StarOne
+ * @Description  : 
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config
+export default defineConfig({
+  plugins: [vue()]
+});
